caee697db1b6baf854a4d7aa5f8b0b688c5e07d1
edel-david <david.edel@gmx.de>
2026-03-21 15:43:10 +0100

    init realnvpSummary on small dataset. tested dropout

--- models/inn.py
+++ models/inn.py
@@ -3,8 +3,7 @@ import torch
 import torch.nn as nn
 from typing import Optional
 from torch.nn.functional import one_hot
-
-import torch.nn.utils as utils
+from .regressionNetwork import RegressionNetwork
 
 from torch.amp.grad_scaler import GradScaler
 
@@ -29,6 +28,7 @@ class CouplingBlock(nn.Module):
             nn.Tanh(),
             # remember to exp this
         )
+        self.cond_drop = nn.Dropout(0.1)
         self.t_net = nn.Sequential(
             nn.Linear(input_size - input_size // 2 + condition_size, hidden_size),
             nn.ReLU(),
@@ -44,7 +44,8 @@ class CouplingBlock(nn.Module):
         # assume x is of shape (batch_size, input_size)
         x1, x2 = torch.split(x, [x.size(1) - x.size(1) // 2, x.size(1) // 2], dim=1)
         # creates views!
-        x1_cond = torch.cat([x1, y], dim=1)
+        y_drop = self.cond_drop(y)
+        x1_cond = torch.cat([x1, y_drop], dim=1)
         scaled_x1 = self.scale_net(x1_cond)
         z2 = x2 * torch.exp(scaled_x1) + self.t_net(x1_cond)
         # could be exp(a * ...), but also for backwards.
@@ -74,6 +75,73 @@ class RealNVP(nn.Module):
         self.input_size = input_size
         self.condition_size = condition_size
         self.blocks: nn.ModuleList[CouplingBlock] = nn.ModuleList()
+        self.rotation_matrices = []
+        for i in range(blocks): # blocks -1 
+            self.blocks.append(CouplingBlock(input_size, hidden_size, condition_size))
+            R = get_orthonormal_matrix(input_size)  # R means Rotational matrix,
+            # could also be named Q.
+            R.requires_grad = False  # prof said its not worth to learn (probably)
+            # but it seams to decrease train loss. # but reconstruction is not possible if R becomes not Orthonormal
+            self.rotation_matrices.append(R)
+
+        #!TODO: readd
+        #self.blocks.append(CouplingBlock(input_size, hidden_size, condition_size))
+
+    def forward(self, x, y=torch.ones((0,), device=device)):
+        """
+        y should be a onehot
+        """
+        for block, R in zip(
+            self.blocks, self.rotation_matrices
+        ):  # pyright: ignore[reportAssignmentType]
+            block: CouplingBlock
+            # last from blocks will not be used in this loop
+            x = block.forward(x, y)
+            x = x @ R  # rotation
+        #!TODO: readd
+        # x = self.blocks[-1].forward(x, y)
+        
+        return x
+
+    def reverse(self, z: torch.Tensor, y: torch.Tensor = torch.ones((0,))):
+        #!TODO: readd
+        # x = z
+        # x = self.blocks[-1].reverse(z, y)
+        for block, R in zip(
+            reversed(self.blocks), reversed(self.rotation_matrices) # self.blocks[:-1]
+        ):
+            block: CouplingBlock
+            x = x @ R.T
+            x = block.reverse(x, y)
+        return x
+
+    def sample(self, num_samples: int, conditions: Optional[torch.Tensor] = None):
+        effective_num_samples = (
+            num_samples if conditions is None else num_samples * conditions.size(0)
+        )
+        gaussians = torch.randn(effective_num_samples, self.input_size, device=device)
+        if conditions is not None:  # conditions is provided
+            conditions = conditions.repeat(num_samples, 1)
+        else:  # conditions is not provided
+            if self.condition_size == 0:  # stack empty tensor
+                conditions = torch.ones((num_samples, 0), device=device)
+            else:  # is a conditional model, but no classes were supplied, so draw random
+                conditions = torch.randint(
+                    0, self.condition_size, (num_samples,), device=device
+                )
+        return self.reverse(gaussians, conditions)
+
+
+class RealNVPsingle(nn.Module):
+    condition_size: int
+
+    def __init__(
+        self, input_size: int = 1, hidden_size: int = 32, blocks: int = 8, condition_size: int = 0
+    ):
+        super(RealNVP, self).__init__()
+        self.input_size = input_size
+        self.condition_size = condition_size
+        self.blocks: nn.ModuleList["CouplingBlockSingle"] = nn.ModuleList() #TODO:
         self.rotation_matrices = []
         for i in range(blocks - 1):
             self.blocks.append(CouplingBlock(input_size, hidden_size, condition_size))
@@ -130,75 +198,43 @@ class RealNVP(nn.Module):
         return self.reverse(gaussians, conditions)
 
 
-def calculate_loss(model, output):
-    loss = torch.zeros((), requires_grad=True, device=device)
-    for block in model.realNVP.blocks:
-        block: CouplingBlock
-        loss -= block.block_det
-    loss += output.pow(2).sum() / 2  # sum over all axis
-    return loss / output.size(0)  # mean over batch
 
 
 
-std_tensor = torch.tensor([...], device=device).reshape((1, 3))
-mean_tensor = torch.tensor([...], device=device).reshape((1, 3))
+# std_tensor = torch.tensor([...], device=device).reshape((1, 3))
+# mean_tensor = torch.tensor([...], device=device).reshape((1, 3))
 
 
-def y_scaling(y: torch.Tensor):
-    # y shape: bs x 3
-    return (y - mean_tensor) / std_tensor
+# def y_scaling(y: torch.Tensor):
+#     # y shape: bs x 3
+#     return (y - mean_tensor) / std_tensor
 
 
-def y_unscale(y_scaled: torch.Tensor):
-    return y_scaled * std_tensor + mean_tensor
+# def y_unscale(y_scaled: torch.Tensor):
+#     return y_scaled * std_tensor + mean_tensor
 
 
-def train_inn_cond(
-    model: nn.Module,
-    train_set_fn,
-    optim: torch.optim.Optimizer,
-    scaler: GradScaler,
-    lr_scheduler :torch.optim.lr_scheduler.LRScheduler,
-    epochs,
-    history,
-    batch_size=128,
-):
-    """
-    model: A inn
-    train_set_fn: a function that returns X,Y
-    with X: the condition (like observations that will be available at inference time)
-    Y: the hidden parameter that we want a posterior for later.
+class RealNVPSummary(nn.Module):
+    def __init__(self,input_size = 200, condition_size = 100 , reduced_condition_size = 8,s_hidden = 1000,s_layers=6, r_hidden = 500,r_blocks=6, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.summary = RegressionNetwork(condition_size,hidden_size= s_hidden, layers=s_layers, output_size=reduced_condition_size)
+        self.realNVP = RealNVP(input_size=input_size,hidden_size=r_hidden,blocks=r_blocks,condition_size=reduced_condition_size)
 
-    # scaler = GradScaler("cuda", enabled=(device.type == 'cuda'))
-    """
-    history["train_loss"] = history.get("train_loss", [])
-    model.train()
-
-    for epoch in range(epochs):
-        epoch_loss = torch.zeros((), requires_grad=False, device=device)
-        epoch_length = 100
-        # for now, define a epoch as 100 iterations, because
-        # we have no train set size yet.
-        for i in range(epoch_length):
-            X, Y = train_set_fn(batch_size)
-
-            X = torch.tensor(
-                X.reshape(batch_size, -1), device=device, dtype=torch.float32
-            )
-            Y = torch.tensor(Y, device=device, dtype=torch.float32)
-            optim.zero_grad()
-            with torch.amp.autocast("cuda", enabled=(device.type == "cuda")):
-                output = model.forward(X, Y)
-                # x is used for condition, y is input (see docstring)
-                loss = calculate_loss(model, output)
-            scaler.scale(loss).backward()
-            scaler.unscale_(optim)
-            utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-            scaler.step(optim)
-            scaler.update()
-            epoch_loss += loss.mean()
-        
-        avg_epoch_loss = (epoch_loss / epoch_length).item()
-        print(f"Epoch {epoch+1}/{epochs}, Loss: {avg_epoch_loss:.4f}", end="\r")
-        history["train_loss"].append(avg_epoch_loss)
-    return
+    def forward(self,x,y = None):
+        """
+        x is observation, y is [lam,mu,i0]
+        """
+        hy = self.summary(y)
+        codes = self.realNVP(x,hy)
+        return codes
+    def reverse(self,z,y):
+        hy = self.summary(y)
+        y = self.realNVP.reverse(z,hy)
+        return y
+    
+    def sample(self,x,n):
+        """
+        samples n per condition
+        """
+        hx = self.summary(x)
+        return self.realNVP.sample(n,hx)
